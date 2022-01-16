627c1f073bc2897001fb50336423d6ff3d2abbea
Shuhaibm <mesm.shuhaib@gmail.com>
2022-01-16 02:43:32 -0800

    Reset to old commit

---
+++ Chat/client.tsx
@@ -0,0 +1,10 @@
+import { ChatClient } from '@azure/communication-chat';
+import { AzureCommunicationTokenCredential } from '@azure/communication-common';
+
+// Your unique Azure Communication service endpoint
+let endpointUrl = 'https://<RESOURCE_NAME>.communication.azure.com';
+// The user access token generated as part of the pre-requisites
+let userAccessToken = '<USER_ACCESS_TOKEN>';
+
+let chatClient = new ChatClient(endpointUrl, new AzureCommunicationTokenCredential(userAccessToken));
+console.log('Azure Communication Chat client created!');
